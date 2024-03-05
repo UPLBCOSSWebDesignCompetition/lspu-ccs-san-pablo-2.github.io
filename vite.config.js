845ecbc4fa080afd3f9cc0c3c2c5299f61b0452a
UPLBCOSSWebDesignCompetition <uplbcosswebdesigncompetition@gmail.com>
2024-03-05 20:45:03 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://uplbcosswebdesigncompetition.github.io/lspu-ccs-san-pablo-2.github.io/',
   plugins: [react()],
 })
